a5dc401a656fd6dc95e367f97f9a5afe23c57000
Ana Cristina Silveira <ana.cris372@gmail.com>
2024-05-16 22:46:49 -0300

    creating migrate

--- knexfile.js
+++ knexfile.js
@@ -4,11 +4,21 @@ module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: path.resolve(__dirname, "src", "database", "database.db")
+      filename: path.resolve(__dirname, "src", "database", "database.db"),
+    },
+    pool:{
+      //conn = conexão | cb = callBack
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON",cb)
     },
     migrations: {
-      directory: path.resolve(__dirname,"src", "database", "knex", "migrations")
+      directory: path.resolve(
+        __dirname,
+        "src",
+        "database",
+        "knex",
+        "migrations"
+      ),
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
   },
 };
